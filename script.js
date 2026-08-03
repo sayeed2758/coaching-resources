f3b02fc5847f8a1838d82180c8b5cb3be9f9e43e
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-04 05:07:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,99 @@
+const data = {
+
+"6":{
+
+"Mathematics":[
+"Knowing Our Numbers",
+"Whole Numbers",
+"Playing With Numbers"
+],
+
+"Science":[
+"Food: Where Does It Come From?",
+"Components of Food",
+"Fibre to Fabric"
+],
+
+"English":[
+"Honeysuckle",
+"A Pact With The Sun"
+],
+
+"Hindi":[
+"वसंत",
+"बाल रामकथा"
+]
+
+},
+
+"7":{
+
+"Mathematics":["Integers","Fractions"],
+
+"Science":["Nutrition in Plants","Nutrition in Animals"]
+
+},
+
+"8":{},
+
+"9":{},
+
+"10":{}
+
+};
+
 function openClass(cls){
 
-let html="";
-
-html+="<h2>Class "+cls+"</h2>";
+let html="<h2>Class "+cls+"</h2>";
 
 html+="<div class='class-grid'>";
 
-html+="<div class='class-card'>Mathematics</div>";
+for(let subject in data[cls]){
 
-html+="<div class='class-card'>Science</div>";
+html+="<div class='class-card' onclick=\"openSubject('"+cls+"','"+subject+"')\">"+subject+"</div>";
 
-html+="<div class='class-card'>English</div>";
-
-html+="<div class='class-card'>Hindi</div>";
-
-html+="<div class='class-card'>Social Science</div>";
-
-html+="<div class='class-card'>Sanskrit</div>";
+}
 
 html+="</div>";
 
 document.getElementById("content").innerHTML=html;
 
-window.scrollTo({
-top:600,
-behavior:"smooth"
+}
+
+function openSubject(cls,subject){
+
+let html="<h2>"+subject+"</h2>";
+
+html+="<div class='class-grid'>";
+
+data[cls][subject].forEach(chapter=>{
+
+html+="<div class='class-card' onclick=\"openChapter('"+chapter+"')\">"+chapter+"</div>";
+
 });
+
+html+="</div>";
+
+document.getElementById("content").innerHTML=html;
+
+}
+
+function openChapter(chapter){
+
+let html="<h2>"+chapter+"</h2>";
+
+html+="<div class='class-grid'>";
+
+html+="<a class='class-card' target='_blank' href='https://drive.google.com/'>📄 Notes</a>";
+
+html+="<a class='class-card' target='_blank' href='https://drive.google.com/'>📊 PPT</a>";
+
+html+="<a class='class-card' target='_blank' href='https://drive.google.com/'>📝 Short Notes</a>";
+
+html+="<a class='class-card' target='_blank' href='https://drive.google.com/'>📚 Worksheet</a>";
+
+html+="</div>";
+
+document.getElementById("content").innerHTML=html;
 
 }
