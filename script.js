c4b99b741743466d7b51173d44a08d6e32326d72
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-04 05:04:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// JavaScript will be added later
+function openClass(cls){
+
+let html="";
+
+html+="<h2>Class "+cls+"</h2>";
+
+html+="<div class='class-grid'>";
+
+html+="<div class='class-card'>Mathematics</div>";
+
+html+="<div class='class-card'>Science</div>";
+
+html+="<div class='class-card'>English</div>";
+
+html+="<div class='class-card'>Hindi</div>";
+
+html+="<div class='class-card'>Social Science</div>";
+
+html+="<div class='class-card'>Sanskrit</div>";
+
+html+="</div>";
+
+document.getElementById("content").innerHTML=html;
+
+window.scrollTo({
+top:600,
+behavior:"smooth"
+});
+
+}
