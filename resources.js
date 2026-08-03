b168f2d04bb56a97ecc6692e6e51747256707959
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-04 05:08:56 +0530

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,40 @@
+const resources = {
+
+  "6": {
+    "Mathematics": {
+
+      "Chapter 1 - Knowing Our Numbers": {
+        notes: "https://drive.google.com/notes-link",
+        ppt: "https://drive.google.com/ppt-link",
+        shortnotes: "https://drive.google.com/short-link",
+        worksheet: "https://drive.google.com/worksheet-link"
+      },
+
+      "Chapter 2 - Whole Numbers": {
+        notes: "https://drive.google.com/notes-link",
+        ppt: "https://drive.google.com/ppt-link",
+        shortnotes: "https://drive.google.com/short-link",
+        worksheet: "https://drive.google.com/worksheet-link"
+      }
+
+    },
+
+    "Science": {
+
+      "Chapter 1 - Food: Where Does It Come From?": {
+        notes: "",
+        ppt: "",
+        shortnotes: "",
+        worksheet: ""
+      }
+
+    }
+
+  },
+
+  "7": {},
+  "8": {},
+  "9": {},
+  "10": {}
+
+};
