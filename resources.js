981b57f88f8cbb70b6d8048983c07f361bcdf875
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-04 05:35:36 +0530

    Update resources.js

--- resources.js
+++ resources.js
@@ -1,40 +1,84 @@
 const resources = {
 
-  "6": {
-    "Mathematics": {
+"6":{
 
-      "Chapter 1 - Knowing Our Numbers": {
-        notes: "https://drive.google.com/notes-link",
-        ppt: "https://drive.google.com/ppt-link",
-        shortnotes: "https://drive.google.com/short-link",
-        worksheet: "https://drive.google.com/worksheet-link"
-      },
+"Mathematics":{
 
-      "Chapter 2 - Whole Numbers": {
-        notes: "https://drive.google.com/notes-link",
-        ppt: "https://drive.google.com/ppt-link",
-        shortnotes: "https://drive.google.com/short-link",
-        worksheet: "https://drive.google.com/worksheet-link"
-      }
+"Chapter 1 - Knowing Our Numbers":{
 
-    },
+notes:"https://drive.google.com/your-notes-link",
 
-    "Science": {
+ppt:"https://drive.google.com/your-ppt-link",
 
-      "Chapter 1 - Food: Where Does It Come From?": {
-        notes: "",
-        ppt: "",
-        shortnotes: "",
-        worksheet: ""
-      }
+shortnotes:"https://drive.google.com/your-shortnotes-link",
 
-    }
+worksheet:"https://drive.google.com/your-worksheet-link"
 
-  },
+},
 
-  "7": {},
-  "8": {},
-  "9": {},
-  "10": {}
+"Chapter 2 - Whole Numbers":{
+
+notes:"",
+
+ppt:"",
+
+shortnotes:"",
+
+worksheet:""
+
+}
+
+},
+
+"Science":{
+
+"Chapter 1 - Food: Where Does It Come From?":{
+
+notes:"",
+
+ppt:"",
+
+shortnotes:"",
+
+worksheet:""
+
+}
+
+},
+
+"English":{},
+"Maths":{},
+"Social Science":{},
+"Science":{}
+
+},
+
+"7":{
+"Mathematics":{},
+"Science":{},
+"English":{},
+"Social Science":{},
+},
+
+"8":{
+"Mathematics":{},
+"Science":{},
+"English":{},
+"Social Science":{},
+},
+
+"9":{
+"Mathematics":{},
+"Science":{},
+"English":{},
+"Social Science":{}
+},
+
+"10":{
+"Mathematics":{},
+"Science":{},
+"English":{},
+"Social Science":{}
+}
 
 };
